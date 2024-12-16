e36785e19de94b710b46271d759c98b82fd18c70
tato <mosidzetato@gmail.com>
2024-12-16 20:14:59 +0400

    added models and costum admin interfaces

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from shop.models import *
+from django.contrib.contenttypes.admin import GenericTabularInline 
 
-# Register your models here.
+class ItemInLine(admin.StackedInline):
+    model = Item 
+    extra = 1
+
+class ItemInLine1(admin.StackedInline):
+    model = Item.tags.through
+    extra = 1
+
+class TagInLine(admin.StackedInline):
+    model = Tag.items.through
+    extra = 1
+
+#---------------------------------------
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','description']
+    search_fields = ['name']
+    ordering = ['name']
+    inlines = [ItemInLine]
+
+class  ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'description']
+    search_fields = ['name','description']
+    ordering = ['-price']
+    inlines = [ItemInLine1]
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    inlines = [TagInLine]
+
+#----------------------------
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Tag, TagAdmin)
+# admin.site.register(Image)
--- shop/models.py
+++ shop/models.py
@@ -1,23 +1,12 @@
 from django.db import models
-from django.db.models import Count
-
-class CategoryManager(models.Manager):
-    def with_item_count(self):
-        return self.annotate(item_count=Count("items"))
-
-class ItemManager(models.Manager):
-    def with_tag_count(self):
-        return self.annotate(tags_count=Count("tags"))
-
-class TagManager(models.Manager):
-    def popular_tags(self, min_items):
-        return self.annotate(items_count=Count("items")).filter(items_count__gt=min_items)
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    categoriesm = CategoryManager()
+
     def __str__(self):
         return self.name
 
@@ -26,7 +15,6 @@ class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
-    itemsm = ItemManager()
 
     def __str__(self):
         return self.name
@@ -34,6 +22,6 @@ class Item(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     items = models.ManyToManyField(Item, related_name='tags', blank=True)
-    tagm = TagManager()
+
     def __str__(self):
         return self.name
